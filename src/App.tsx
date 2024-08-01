159eea0bea38b4bd55326923c79d62afb85cb27e
Jayesh Deorukhkar <jddeorukhkar@gmail.com>
2024-08-01 17:02:17 +0530

    fix: errors in build

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import HtmlPreview from "./HtmlPreview";
 import HtmlEditor from "./HtmlEditor";
 import JsonEditor from "./JsonEditor";
 import Footer from "./Footer";
-import React from 'react'
 
 // const virtualConsole = new jsdom.VirtualConsole();
 // virtualConsole.on("error", () => {
