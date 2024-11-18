c4f1262a11563930609bb13acf8616bee3084f30
Jayesh Deorukhkar <jddeorukhkar@gmail.com>
2024-11-19 01:06:34 +0530

    add HTML emmet support

--- src/HtmlEditor.tsx
+++ src/HtmlEditor.tsx
@@ -1,5 +1,5 @@
 import { Editor } from '@monaco-editor/react';
-import prettify from "pretty";
+import { emmetHTML, emmetCSS } from "emmet-monaco-es";
 
 function HtmlEditor({html, onChange, height}: {html: string, onChange?: (s: string) => void, height?: string}) {
   return (
@@ -9,7 +9,11 @@ function HtmlEditor({html, onChange, height}: {html: string, onChange?: (s: stri
           height= {height ?? "50vh" }
           theme="vs-dark"
           language="html"
-          value={prettify(html)}
+          value={html}
+          onMount={(editor, monaco) => {
+            emmetHTML(monaco);
+            emmetCSS(monaco);
+          }}
           onChange={(s: any) => {
             if (!s) return;
             onChange?.(s);
